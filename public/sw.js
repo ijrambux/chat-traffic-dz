ff357d816b6894cf18da7ec9704bd733cc82edbc
The Cook Rat <ijrambux@gmail.com>
2026-02-07 00:47:29 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
+// public/sw.js
 self.options = {
     "domain": "3nbf4.com",
     "zoneId": 10579000
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+};
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
